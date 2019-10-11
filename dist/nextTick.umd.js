e4675c6040a5ccc571d4ed51e97c199fc2625b0b
Dmitry Vibe <riim@yandex.ru>
2019-10-11 15:40:18 +0300

    add umd bundle

---
+++ dist/nextTick.umd.js
@@ -0,0 +1,30 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(global = global || self, factory(global['@riim/next-tick'] = {}));
+}(this, function (exports) { 'use strict';
+
+	const nextTick = (() => {
+	    const global = Function('return this;')();
+	    if (global.process &&
+	        global.process.toString() == '[object process]' &&
+	        global.process.nextTick) {
+	        return global.process.nextTick;
+	    }
+	    if (global.setImmediate && global.setImmediate.toString().indexOf('[native code]') != -1) {
+	        const setImmediate = global.setImmediate;
+	        return (cb) => {
+	            setImmediate(cb);
+	        };
+	    }
+	    const promise = Promise.resolve();
+	    return (cb) => {
+	        promise.then(cb);
+	    };
+	})();
+
+	exports.nextTick = nextTick;
+
+	Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import tslint from 'rollup-plugin-tslint';
+import typescript from 'rollup-plugin-typescript2';
+
+export default {
+	input: './src/index.ts',
+
+	output: {
+		file: './dist/nextTick.umd.js',
+		format: 'umd',
+		name: '@riim/next-tick'
+	},
+
+	plugins: [
+		tslint(),
+		typescript({ clean: true })
+	]
+};
