4715ddd8cd56ac524a7251b2bd706df1662971db
Dmitry Vibe <riim@yandex.ru>
2021-03-31 18:51:19 +0300

    use commonjs type instead of module

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@ import tslint from 'rollup-plugin-tslint';
 import typescript from 'rollup-plugin-typescript2';
 
 export default {
-	input: './src/index.ts',
+	input: './src/nextTick.ts',
 
 	output: {
 		file: './dist/nextTick.umd.js',
